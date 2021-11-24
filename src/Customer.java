f8b5b205fd8106564f43b3ae2a0f1acdaaa41e69
Jackie Dallas <jackie.r.dallas@gmail.com>
2021-11-23 21:19:09 -0600

    created customer class

---
+++ src/Customer.java
@@ -0,0 +1,12 @@
+public class Customer {
+    // Properties
+    String firstName;
+    String lastName;
+    int age;
+
+    public Customer(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+}
